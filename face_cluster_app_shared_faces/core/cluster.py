59c1a8e9c52e10af3e525ab6d8f5e3fbb56eb39e
RockInMyHead <65908604+RockInMyHead@users.noreply.github.com>
2025-09-10 13:55:53 +0300

    Add files via upload

--- face_cluster_app_shared_faces/core/cluster.py
+++ face_cluster_app_shared_faces/core/cluster.py
@@ -2,7 +2,7 @@
 from insightface.app import FaceAnalysis
 import numpy as np
 from pathlib import Path
-from PIL import Image
+import cv2
 from sklearn.cluster import DBSCAN
 from tqdm import tqdm
 
@@ -11,7 +11,7 @@ IMG_EXTS = {'.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff'}
 def is_image(p: Path):
     return p.suffix.lower() in IMG_EXTS
 
-def build_plan(input_dir: Path, det_size=(1280, 1280), dbscan_eps=0.45, dbscan_min_samples=2):
+def build_plan(input_dir: Path, det_size=(1024, 1024), dbscan_eps=0.5, dbscan_min_samples=2):
     input_dir = Path(input_dir)
     all_images = [p for p in input_dir.rglob("*") if is_image(p)]
 
@@ -20,19 +20,14 @@ def build_plan(input_dir: Path, det_size=(1280, 1280), dbscan_eps=0.45, dbscan_m
 
     embeddings = []
     img_index = []
-    valid_paths = []
     for img_path in tqdm(all_images, desc="📷 Scanning"):
-        try:
-            img = np.array(Image.open(img_path).convert("RGB"))
-        except Exception:
+        img = cv2.imread(str(img_path))
+        if img is None:
             continue
         faces = model.get(img)
-        if not faces:
-            continue
         for face in faces:
             embeddings.append(face.embedding)
             img_index.append(img_path)
-        valid_paths.append(img_path)
 
     if not embeddings:
         return []
@@ -44,11 +39,11 @@ def build_plan(input_dir: Path, det_size=(1280, 1280), dbscan_eps=0.45, dbscan_m
     cluster_by_img = {}  # path -> list of clusters
     for path, label in zip(img_index, labels):
         if label == -1:
-            continue
+            continue  # нераспознанные
         cluster_by_img.setdefault(path, set()).add(label)
 
     plan = []
-    for path in valid_paths:
+    for path in all_images:
         clusters = cluster_by_img.get(path)
         if not clusters:
             continue
